f271c3fabb91c0baa889a5f090edca6d5de77ea6
Tusif Hassan Rizvi <tausifhassan123@gmail.com>
2022-12-25 14:32:12 +0530

    adding change

--- src/components/Recordaudio.js
+++ src/components/Recordaudio.js
@@ -1,61 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from "react";
+import { saveAs } from 'file-saver';
+
 
 function Recordaudio() {
   const [recording, setRecording] = useState(false);
   const [audioBlob, setAudioBlob] = useState(null);
-  const [recorde, setRecorder]=useState(null);
-  const [isdownload, setIsdownload]=useState(false);
-  
+  const [recorde, setRecorde] = useState(null);
+  const [isdownload, setIsdownload] = useState(false);
+  const [audiolink, setAudiolink] = useState("");
 
   const handleStart = async () => {
     // request access to the user's microphone
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    // start recording
+    // start recordingnpm 
     let recorder = new MediaRecorder(stream);
-    setRecorder(recorder)
+    setRecorde(recorder);
     recorder.start();
 
     // store the recorded audio in a blob
     const chunks = [];
-    recorder.addEventListener('dataavailable', event => {
+    recorder.addEventListener("dataavailable", (event) => {
       chunks.push(event.data);
     });
 
-    recorder.addEventListener('stop', () => {
+    recorder.addEventListener("stop", () => {
       setAudioBlob(new Blob(chunks));
+      setAudiolink(URL.createObjectURL(new Blob(chunks)));
     });
 
     setRecording(true);
-    setIsdownload(false)
+    setIsdownload(false);
   };
 
   const handleStop = () => {
     // stop the recording
-    console.log(recorde)
     recorde.stop();
     setRecording(false);
-    setIsdownload(true)
+    setIsdownload(true);
   };
 
   const handleSave = () => {
-    // prompt the user to select a location to save the file
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(audioBlob);
-    a.download = 'recording.mp3';
-    document.body.appendChild(a);
-    a.click();
+    let fileURL=URL.createObjectURL(audioBlob);
+    let fileName="recording.mp3";
+    saveAs(fileURL, fileName);
   };
 
   return (
-    <div style={{display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"column"}}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
       {!recording && <button onClick={handleStart}>Start Recording</button>}
-      {recording?<h1>recording.....</h1>:null}
+      {recording ? <h1>recording.....</h1> : null}
       {recording && <button onClick={handleStop}>Stop Recording</button>}
-      {isdownload?<h3>click on download button and get recorded file! </h3>:null}
-      {recording?null:audioBlob && <button onClick={handleSave}>Download Recording!</button>}
+      {isdownload ? (
+        <>
+          <h3>click on download button and get recorded file! </h3>
+          <audio controls src={audiolink}>
+          </audio>
+        </>
+      ) : null}
+      {recording
+        ? null
+        : audioBlob && (
+            <button onClick={handleSave}>Download Recording!</button>
+          )}
     </div>
   );
-  }
+}
 
 export default Recordaudio;
